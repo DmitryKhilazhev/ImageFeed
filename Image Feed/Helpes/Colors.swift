2080f878de75caa4e3306063932988e3f4301082
Dmitry Khilazhev <dkhilazhev@mail.ru>
2023-03-12 13:36:14 +0300

    Profile screen made by controller

---
+++ Image Feed/Helpes/Colors.swift
@@ -0,0 +1,20 @@
+//
+//  Colors.swift
+//  Image Feed
+//
+//  Created by Dmitry on 12.03.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var ypBackground: UIColor = UIColor(named: "background")!
+    static var ypBlack: UIColor = UIColor(named: "black")!
+    static var ypBlue: UIColor = UIColor(named: "blue")!
+    static var ypGray: UIColor = UIColor(named: "gray")!
+    static var ypRed: UIColor = UIColor(named: "red")!
+    static var ypWhite: UIColor = UIColor(named: "white")!
+    static var ypWhiteIOS: UIColor = UIColor(named: "whiteIOS")!
+    static var imageStartGradient: UIColor = UIColor(named: "imageStartGradient")!
+    static var imageEndGradient: UIColor = UIColor(named: "imageEndGradient")!
+}
