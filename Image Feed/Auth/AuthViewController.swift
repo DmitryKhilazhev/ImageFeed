93a78a7144f7304b917da84f8ce4986f0942dbf8
Dmitry Khilazhev <dkhilazhev@mail.ru>
2023-03-21 20:09:55 +0900

    before progress view

---
+++ Image Feed/Auth/AuthViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AuthViewController.swift
+//  Image Feed
+//
+//  Created by Dmitry on 19.03.2023.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    private let ShowWebViewSegueIdentifier = "ShowWebView"
+}
+
---
+++ Image Feed/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Image Feed
+//
+//  Created by Dmitry on 19.03.2023.
+//
+
+import Foundation
+
+let AccessKey = "V5Y17PFQR4tHEfHN7mTWYhnw4zAaZYLRrmEVwtWHN5Y"
+let SecretKey = "hnxV_7O24bvZ_MU9pCZCqkTMrAaEUyxfWnWgeGtplUo"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
